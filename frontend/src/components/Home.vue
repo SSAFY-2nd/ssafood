c08e16002c9141e3a19c0b4f792315aa6e21747c
김정현 <jh936@naver.com>
2020-09-17 22:17:01 +0900

    jeonghyun : first
    Former-commit-id: 1a06bc3f053358ca649d3639f0f9fdfbc28e3b82

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,7 +2,147 @@
   <div id=app>
     <!-- 네비게이션 바 -->
     <Navbar></Navbar>
-    <div class="toptab">
+    <section id="intro">
+      <div id="intro-background"></div>
+      <div id="intro-tagline">
+        
+        <h1>맛있는 음식점을 한눈에!</h1>
+        <h1>SSAFOOD!</h1>
+     
+        <v-container fill-height fluid grid-list-xl>
+      <v-layout justify-center wrap mt-5>
+        <v-flex xs12 md8>
+          <card title="맛집 검색">
+            <v-form>
+              <v-container py-0>
+                <v-layout wrap>
+                  <v-flex xs12 md12>
+                    <v-text-field v-model="storeName" label="음식점 이름" />
+                  </v-flex>
+                  <v-flex xs12 text-center>
+                    <v-btn
+                      large
+                      class="indigo white--text ma-5"
+                      rounded
+                      color="orange darken-1"
+                      @click="onSubmit"
+                    >GO!</v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-form>
+          </card>
+          <v-divider class="mx-4" />
+        </v-flex>
+        <v-flex xs12 md8>
+          <v-flex v-for="store in stores" :key="store.id" pa-4>
+            <store-list-card
+              :id="store.id"
+              :name="store.name"
+              :categories="store.categories"
+              :address="store.address"
+              :tel="store.tel"
+            />
+          </v-flex>
+        </v-flex>
+      </v-layout>
+    </v-container>
+     </div>
+    </section>
+    
+    <main class="page-content">
+    <div class="page-container">
+        <h1>About SSAFOOD</h1>
+        <hr />
+        <h2>당신이 가고 싶은 음식점은 어디?</h2>
+
+        <p>음식 추천 사이트</p>
+        
+        </div>
+        <section id="services" class="sec-services">
+        <div class="container">
+          <h1>Services</h1>
+          <hr/>
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="col" style="right:30px">
+                <v-icon size="100" >mdi-kabaddi</v-icon>
+              </div>
+              <h2 class="h3"> 맛집 추천</h2>
+              <p>카테고리 별로 맛집을 추천해줍니다.</p>
+            </div>
+            <div class="col-sm-4">
+              <div class="col" style="right:30px">
+                <v-icon size="100" >mdi-calendar-clock</v-icon>
+              </div>
+              <h2 class="h3">다이어리</h2>
+              <p>나의 맛집 방문기록을 확인하세요</p>
+            </div>
+            <div class="col-sm-4">
+              <div class="col" style="right:30px">
+                <v-icon size="100" >mdi-nutrition</v-icon>
+              </div>
+              <h2 class="h3">식단</h2>
+              <p>적절한 칼로리의 식단을 제공합니다</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="folio" class="sec-folio">
+        <div class="container">
+          <h1>평점 높은 음식들</h1>
+          <hr />
+          <div class="row">
+            <div class="col-md-4">
+              <img class="center-block" src="../assets/img/순대국.png" alt="By Håkon Sataøen" /><br>
+              <v-icon size="20">mdi-account-circle</v-icon> 순대천국 <br>
+              <v-icon size="20">mdi-badge-account-horizontal </v-icon> 서울시 노원구 <br>
+              <v-icon size="20">mdi-cellphone-iphone</v-icon> 010 1357 2468           
+            </div>
+            <div class="col-md-4">
+              <img class="center-block" src="../assets/img/육회.png" alt="By Samuel Zeller" /><br>
+               <v-icon size="20">mdi-account-circle</v-icon> 육회천국 <br>
+              <v-icon size="20">mdi-badge-account-horizontal </v-icon> 서울시 노원구 <br>
+              <v-icon size="20">mdi-cellphone-iphone</v-icon> 010 1357 2468      
+            </div>
+            <div class="col-md-4">
+              <img class="center-block" src="../assets/img/조각케이크.png" alt="By Scott Webb" /><br>
+               <v-icon size="20">mdi-account-circle</v-icon> 케이크천국 <br>
+              <v-icon size="20">mdi-badge-account-horizontal </v-icon> 서울시 노원구 <br>
+              <v-icon size="20">mdi-cellphone-iphone</v-icon> 010 1357 2468   
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <img class="center-block" src="../assets/img/조각피자.png" alt="By Daniel Lozano Valdés" /><br>
+              <v-icon size="20">mdi-account-circle</v-icon> 피자공주 <br>
+              <v-icon size="20">mdi-badge-account-horizontal </v-icon> 서울시 노원구 <br>
+              <v-icon size="20">mdi-cellphone-iphone</v-icon> 010 1357 2468   
+            </div>
+            <div class="col-md-4">
+              <img class="center-block" src="../assets/img/치킨.png" alt="By Scott Webb" /><br>
+               <v-icon size="20">mdi-account-circle</v-icon> 치킨나라 <br>
+              <v-icon size="20">mdi-badge-account-horizontal </v-icon> 서울시 노원구 <br>
+              <v-icon size="20">mdi-cellphone-iphone</v-icon> 010 1357 2468    
+            </div>
+            <div class="col-md-4">
+              <img class="center-block" src="../assets/img/햄버거.png" alt="By Daniel Lozano Valdés" /><br>
+               <v-icon size="20">mdi-account-circle</v-icon> 햄버거파티 <br>
+              <v-icon size="20">mdi-badge-account-horizontal </v-icon> 서울시 노원구 <br>
+              <v-icon size="20">mdi-cellphone-iphone</v-icon> 010 1357 2468   
+            </div>
+          </div>
+        </div>
+      </section>
+      </main>
+
+
+
+
+    
+    
+    <!-- <div class="toptab">
       <div class="doasis" 
         data-aos="fade-up" 
         data-aos-offset="50"
@@ -32,7 +172,9 @@
           <div>{{ item }}</div>
         </v-tab>
       </v-tabs>
-    </div>
+    </div> --> 
+
+
     <div class="contain">
       <div class="content_area">
         <!-- 게시글 탭 -->
@@ -101,6 +243,8 @@ import axios from 'axios'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import InfiniteLoading from 'vue-infinite-loading';
+import Card from "@/components/Card";
+import StoreListCard from "@/components/StoreListCard";
 
 AOS.init();
 
@@ -146,6 +290,8 @@ export default {
   components: {
     InfiniteLoading,
     Navbar,
+    Card,
+    StoreListCard
   },
   methods: {
     postdetail(one_post) {
@@ -302,4 +448,185 @@ em {
   float:right; 
   margin-top: -24px
 }
+
+
+
+
+
+body {
+    font-size: 100%;
+    font-family: "Open Sans", sans-serif;
+    color: #f35535;
+    background-color: #FB8C00;
+}
+html * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+body {
+    line-height: 1;
+}
+
+
+#intro {
+    position: absolute;
+    /*position: fixed;*/
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 700px;
+    margin-top: 0;
+}
+
+#intro #intro-background {
+    height: 100%;
+    width: 100%;
+    /* background: url("http://www.hdwallpapers.in/walls/milky_way_sky-wide.jpg") no-repeat center center; */
+    background: url("../assets/img/food.png") no-repeat center center;
+    /* background: url("../assets/img/benchpress_wallpaper.jpg") no-repeat center center; */
+    background-size: cover;
+    box-shadow: 0 0 30px rgba(0, 0, 0, 1);
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+#intro #intro-tagline{
+    position: absolute;
+    width: 90%;
+    max-width: 1170px;
+    left: 50%;
+    top: 50%;
+    bottom: auto;
+    right: auto;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    -moz-transform: translateX(-50%) translateY(-50%);
+    -ms-transform: translateX(-50%) translateY(-50%);
+    -o-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+    color: white;
+    font-size: 20px;
+    font-size: 1.5rem;
+    font-weight: 300;
+    text-align: center;
+}
+
+#intro h1 {
+    font-family: "Open Sans", helvetica, arial, sans-serif;
+    text-transform: uppercase;
+    font-size: 1.8em;
+    font-weight: 300;
+    text-align: center;
+    color: #ffffff;
+    text-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+}
+
+.page-content {
+    position: relative;
+    padding: 2em 0;
+    line-height: 1.8;
+    color: #65676f;
+    font-weight: 100;
+    /* background-color: #E6F6F4; */
+    background-color: #ffffff;
+    z-index: 2;
+}
+
+.page-content::before {
+    content: '';
+    position: absolute;
+    bottom: 100%;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    background: -webkit-linear-gradient( bottom , rgba(46, 49, 61, 0.5), rgba(46, 49, 61, 0));
+    background: linear-gradient(to top, rgba(46, 49, 61, 0.5), rgba(46, 49, 61, 0));
+}
+
+@media only screen and (min-width: 1170px) {
+.page-content {
+    padding: 4em 0;
+    margin-top: 570px;
+ }
+}
+
+@media only screen and (min-width: 768px) { 
+.cd-content {
+    line-height: 1.8;
+ }
+}
+
+.page-container {
+    width: 90%;
+    max-width: 768px;
+    margin: 0 auto;
+}
+
+.page-container::after {
+    content: '';
+    display: table;
+    clear: both;
+}
+
+@media only screen and (min-width: 1170px) {
+.page-content p {
+    font-size: 20px;
+    font-size: 1.25rem;
+ }
+}
+
+@media only screen and (min-width: 768px) {
+.page-content h2 {
+    font-size: 30px;
+    font-size: 1.875rem;
+ }
+}
+
+.page-content h2 {
+    margin: 2em 0;
+    /* font-size: 60px; 20 */
+    /* font-size: 1.25rem; */
+}
+
+.page-content p {
+    margin: 1em 0;
+}
+/* About */
+
+
+/*Services*/
+
+/* .sec-services {
+  background: #f9f9f9;
+} */
+
+.sec-services i {
+  display: block;
+  margin: auto;
+  width: 45px;
+}
+
+.sec-services .fa-paint-brush {
+  width: 64px;
+}
+
+.sec-services .fa-code {
+  width: 68px
+}
+
+.sec-services h2 {
+  margin: 1em 0;
+}
+
+/* image layout*/
+.sec-folio .row {
+  /* margin-top: 2em; */
+  margin-bottom: 1em;
+  font-size: 16px;
+  font-size: 1.00em;
+}
+
+.sec-folio img {
+  max-width: 100%;
+}
+
 </style>
