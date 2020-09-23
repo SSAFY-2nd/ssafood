edd54453865028b33a96c5ca10bf20cc67cda6cc
임건호 <ghlim001@naver.com>
2020-09-23 13:28:26 +0900

    fix
    Former-commit-id: d4849f65d69505266adee94e86801acce9afe26c

---
+++ frontend/src/components/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="Datail">
+        <Navbar></Navbar>
+        <h3></h3>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+export default {
+    
+}
+</script>
