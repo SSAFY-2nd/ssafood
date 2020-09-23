877084faaa8fc39b3cc59d776fd433101839a30a
우주호 <einjuho7@ajou.ac.kr>
2020-09-23 23:38:25 +0900

    Feature : Store & User 수정
    Former-commit-id: b899620b49147b152068abf77f109013ae890e47

---
+++ Backend/backend/backend/src/main/java/com/backend/controller/StoreController.java
@@ -0,0 +1,13 @@
+package com.backend.controller;
+
+
+import com.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StoreController {
+
+
+
+}
