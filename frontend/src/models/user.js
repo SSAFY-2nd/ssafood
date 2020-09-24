e135296a386d1133f2955413f9a327b2625befa4
ru6300 <ru6300@naver.com>
2020-09-24 21:33:56 +0900

    Change Main.Vue
    Former-commit-id: e3f723cf9d2b35e8874bb7fc496141e1b7181663

--- frontend/src/models/user.js
+++ frontend/src/models/user.js
@@ -1,8 +1,10 @@
 export default class User {
-    constructor (username, email, password, confirmPassword) {
+    constructor (username, email, password, confirmPassword, age, gender) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.confirmPassword = confirmPassword;
+        this.age = age;
+        this.gender = gender;
     }
 }
