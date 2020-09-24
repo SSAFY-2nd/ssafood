25b813a857e05b669691398443bc4037461dbf5e
우주호 <einjuho7@ajou.ac.kr>
2020-09-24 10:40:05 +0900

    Fix : Backend 중첩 폴더 삭제
    Former-commit-id: bad64f3efd401cdd48902602d3b64f60b95d95e1

--- Backend/backend/src/main/java/com/backend/dao/UserDao.java
+++ Backend/backend/src/main/java/com/backend/dao/UserDao.java
@@ -1,13 +1,12 @@
-package com.backend.service;
+package com.backend.dao;
 
 import com.backend.dto.user.User;
 
-public interface UserService {
+public interface UserDao {
 
     public void save(User c); // 회원 가입
     public void delete(String uid); // 회원 탈퇴
     public User findByUid(String uid); // 회원 정보 확인
     public User findByEmail(String email); // 회원 정보 확인
     public void update(User c); // 회원 정보 수정
-    User signin(String email, String password);
 }
