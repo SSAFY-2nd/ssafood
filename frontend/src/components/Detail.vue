9c248ec0be4c289918182084cd88fbce735ea8f7
ru6300 <ru6300@naver.com>
2020-09-24 12:58:04 +0900

    S03P23A407-44
    Former-commit-id: a54659761fe13cde57ea9a7daf24f3c593a4c7fe

--- frontend/src/components/Detail.vue
+++ frontend/src/components/Detail.vue
@@ -2,34 +2,95 @@
     <div>
         <Navbar></Navbar>
         <div class="detail">
+            <div calss="header">
+            <img src="../assets/img/food.png" width="300" height="300"> 
+            </div>
             <div class="content">
             <h1 class="rest-title">음식점 이름 들어갈 곳</h1>
             <v-icon slot="append" >mdi-eye</v-icon>조회수
             <v-icon slot="append" >mdi-pencil</v-icon>리뷰수
             <v-icon slot="append" >mdi-star</v-icon>즐겨찾기수
             <hr>
-            <div class="d-flex justify-content-between">
-                    <div>주소</div> 음식점 주소 <br><br>
-                    <div>전화번호</div> 음식점 전화번호<br><br>
-                    <div>주소</div>
-                </div>
-            <br><br>
-                    <br><br>
-            음식종류     음식점 음식종류<br><br>
-            가격대  음식점 가격대<br><br>
-            주차   발렛<br><br>
-            영업 시간   10:00 ~ 24:00<br><br>
-            휴일   일, 둘째 / 넷째 월 <br><br>
-            웹사이트    식당 홈페이지로 가기<br>
-            메뉴  메뉴1 가격<br>
+                <table class = "story">
+                    <tr>
+                        <td>주소</td>
+                        <td><div style="margin-left:150px">음식점 주소</div></td>
+                    </tr>
+                     <div style="margin-top:10px"></div>
+                     <tr>
+                        <td>전화번호</td>
+                        <td><div style="margin-left:150px">음식점 전화번호</div></td>
+                    </tr>
+                     <div style="margin-top:10px"></div>
+                     <tr>
+                        <td>음식 종류</td>
+                        <td><div style="margin-left:150px">음식점 음식 종류</div></td>
+                    </tr>
+                     <div style="margin-top:10px"></div>
+                     <tr>
+                        <td>가격대</td>
+                        <td><div style="margin-left:150px">음식점 가격대</div></td>
+                    </tr>
+                     <div style="margin-top:10px"></div>
+                    <tr>
+                        <td>주차</td>
+                        <td><div style="margin-left:150px">발렛</div></td>
+                    </tr>
+                     <div style="margin-top:10px"></div>
+                      <tr>
+                        <td>영업 시간</td>
+                        <td><div style="margin-left:150px">10:00 ~ 24:00</div></td>
+                    </tr>
+                     <div style="margin-top:10px"></div>
+                     <tr>
+                        <td>휴일</td>
+                        <td><div style="margin-left:150px">일, 둘째 / 넷째 월</div></td>
+                    </tr>
+                     <div style="margin-top:10px"></div>
+                     <tr>
+                        <td>웹사이트</td>
+                        <td><div style="margin-left:150px">식당 홈페이지로 가기</div></td>
+                    </tr>
+                    <div style="margin-top:10px"></div>
+                     <tr>
+                        <td rowspan=3>메뉴</td>
+                        <td><div style="margin-left:150px">메뉴1 가격</div></td>
+                    </tr>
+                    <tr>
+                        <div style="margin-left:150px">메뉴2 가격</div>
+                    </tr>
+                    <tr>
+                        <div style="margin-left:150px">메뉴3 가격</div>
+                    </tr>
+                   
+                </table>
             </div>
             <div class = "right-side">
-                지도<br><br><br><br><br><br><br><br>
+               <div id="map"></div>
+               <br>
                 <h1 class="rest-back">주변 인기 식당</h1><br>
-                <img src="../assets/img/food.png" width="50" height="50"><p>음식점1 <br>hello</p><br>
-                <img src="../assets/img/food2.jpg"  width="50" height="50">음식점2<br>
-                <img src="../assets/img/food3.jpg"  width="50" height="50">음식점3<br>
-                <img src="../assets/img/food4.jpg"  width="50" height="50">음식점3<br>
+                <table>
+                    <tr>
+                        <td rowspan=3><img src="../assets/img/food.png" width="70" height="70"></td><td class="td-header"><div style="margin-left:20px">음식점1</div></td>
+                    </tr>
+                    <tr><div style="margin-left:20px">hello</div></tr>
+                    <tr><div style="margin-left:20px">bye</div></tr>
+                     <tr>
+                        <td rowspan=3><img src="../assets/img/food2.jpg" width="70" height="70"></td><td class="td-header"><div style="margin-left:20px">음식점2</div></td>
+                    </tr>
+                    <tr><div style="margin-left:20px">hello</div></tr>
+                    <tr><div style="margin-left:20px">bye</div></tr>
+                     <tr>
+                        <td rowspan=3><img src="../assets/img/food3.jpg" width="70" height="70"></td><td class="td-header"><div style="margin-left:20px">음식점3</div></td>
+                    </tr>
+                    <tr><div style="margin-left:20px">hello</div></tr>
+                    <tr><div style="margin-left:20px">bye</div></tr>
+                     <tr>
+                        <td rowspan=3><img src="../assets/img/food4.jpg" width="70" height="70"></td><td class="td-header"><div style="margin-left:20px">음식점4</div></td>
+                    </tr>
+                    <tr><div style="margin-left:20px">hello</div></tr>
+                    <tr><div style="margin-left:20px">bye</div></tr>
+                </table>
             </div>
             <div class="footer">
             <h1 class="rest-foot">리뷰(32)</h1>
@@ -56,16 +117,49 @@ export default {
             tel:'',
             }
         }
+    },
+     mounted() {
+        if (window.kakao && window.kakao.maps) {
+            this.initMap();
+        } else {
+            const script = document.createElement('script');
+            /* global kakao */
+            script.onload = () => kakao.maps.load(this.initMap);
+            script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=YOUR_APPKEY';
+            document.head.appendChild(script);
+        }
+    },
+    methods: {
+        initMap() {
+            var container = document.getElementById('map');
+            var options = {
+              center: new kakao.maps.LatLng(33.450701, 126.570667),
+              level: 3
+            };
+
+            var map = new kakao.maps.Map(container, options);
+            map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+        }
     }
 }
 </script>
 
 <style scoped>
+#map {
+    width: 500px;
+    height: 400px;
+}
 .detail {
   padding-top:151px;
   width:1400px;
   margin-left : 100px;
   height : 1500px;
+}
+.story{
+    margin-top : 50px;
+}
+.story tr{
+    margin-top : 10px;
 }
 .content {
     width:900px;
@@ -73,7 +167,7 @@ export default {
     height:700px;
 }
 .header{
-    height:100px;
+    padding :0 0 0 200px;
 }
 .rest-title {
   font-family: 'NanumSquare','나눔스퀘어','Noto Sans','Apple SD Gothic',sans-serif;
@@ -103,7 +197,11 @@ export default {
     float:left;
     height:700px;
 }
-
+.td-header{
+  font-family: 'NanumSquare','나눔스퀘어','Noto Sans','Apple SD Gothic',sans-serif;
+  font-size: 22px;
+  font-weight: 800;
+}
 .rest-foot {
   font-family: 'NanumSquare','나눔스퀘어','Noto Sans','Apple SD Gothic',sans-serif;
   font-size: 26px;
