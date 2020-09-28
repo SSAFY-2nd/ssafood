d3f18ae62a6bb41bfda7e66d379198a13fa6164d
우주호 <einjuho7@ajou.ac.kr>
2020-09-28 19:38:43 +0900

    Feature : Review CRUD
    Former-commit-id: d1852fb4c8494b035b3d2c5810c4b48d810c687e

---
+++ Backend/src/main/java/com/backend/controller/ReviewController.java
@@ -0,0 +1,99 @@
+package com.backend.controller;
+
+
+import com.backend.dto.review.Review;
+import com.backend.dto.store.Store;
+import com.backend.service.BhourService;
+import com.backend.service.ReviewService;
+import com.backend.service.StoreService;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@Slf4j
+public class ReviewController {
+
+    @Autowired
+    StoreService storeService;
+
+    @Autowired
+    BhourService bhourService;
+
+    @Autowired
+    ReviewService reviewService;
+
+    /**
+     * @param review
+     * @return null;
+     */
+    @ApiOperation(value = "review", notes = "Review 작성")
+    @PostMapping("/api/v1/review")
+    public void saveReview(Review review) {
+        try{
+            reviewService.save(review);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+        System.out.println("Review 작성 성공");
+    }
+
+    /**
+     * @param id
+     * @return Review 리턴;
+     */
+    @ApiOperation(value = "review", notes = "Review 읽기")
+    @GetMapping("/api/v1/review/{id}")
+    public Review readReview(@PathVariable Long id) {
+        Review review = null;
+        try{
+            review = reviewService.read(id);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+        System.out.println("Review 조회 성공");
+        return review;
+    }
+
+    /**
+     * @param review
+     * @return null;
+     */
+    @ApiOperation(value = "review", notes = "Review 수정")
+    @PutMapping("/api/v1/review")
+    public void updateReview(Review review) {
+        try{
+            reviewService.update(review);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+        System.out.println("Review 수정 성공");
+    }
+
+    /**
+     * @param review_id
+     * @return null;
+     */
+    @ApiOperation(value = "review_id", notes = "Review 삭제")
+    @DeleteMapping("/api/v1/review/{review_id}")
+    public void deleteReview(@PathVariable Long review_id) {
+        try{
+            System.out.println(review_id);
+            reviewService.delete(review_id);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+        System.out.println("Review 삭제 성공");
+    }
+
+
+}
