65471fb911396d848b4797b34c24e86ead33083a
임건호 <ghlim001@naver.com>
2020-09-29 17:40:35 +0900

    P03S23A407-50 Gunho: create Diary.vue
    Former-commit-id: 79167e8e91ff22c08ef3c94013cd4a0dd2b6d980

---
+++ frontend/src/components/Diary.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+import axios from 'axios'
+
+export default {
+    
+}
+</script>
